a9f7f1197f61bbb2cf84dff9706140a07ffb12c6
bekzat <tursunkulovbeka@gmile.com>
2023-05-07 18:04:41 +0600

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ using System.Threading;
 
 class Program {
     static void Main(string[] args) {
-        var x1 = GetArray(1000);
+        var x1 = GetArray(100000);
         var x2 = GetArray(1000000);
         var x3 = GetArray(10000000);
         Check(x1);
@@ -18,48 +18,37 @@ class Program {
     private  static void Check(int[] x) {
         var timer = new Stopwatch();
         Console.WriteLine($"array lenght: {x.Length}");
+
         timer.Start();
-        try {
-            Sum(x);
-            timer.Stop();
-            Console.WriteLine("Simple " + timer.ElapsedMilliseconds);
-        } catch {
-            timer.Stop();
-            Console.WriteLine("error");
-        }
+        Sum(x);
+        timer.Stop();
+        Console.WriteLine("Simple " + timer.ElapsedMilliseconds);
         timer.Reset();
 
 
         timer.Start();
-        try {
-            ThreadSum(x);
-            timer.Stop();
-            Console.WriteLine("Thread " + timer.ElapsedMilliseconds);
-        }catch {
-            timer.Stop();
-            Console.WriteLine("error");
-        }
+        ThreadSum(x);
+        timer.Stop();
+        Console.WriteLine("Thread " + timer.ElapsedMilliseconds);
         timer.Reset();
 
         timer.Start();
-        try {
-            ParallelSum(x);
-            timer.Stop();
-            Console.WriteLine("Parallel " + timer.ElapsedMilliseconds);
-        }catch {
-            timer.Stop();
-            Console.WriteLine("error");
-        }
+        ParallelSum(x);
+        timer.Stop();
+        Console.WriteLine("Parallel " + timer.ElapsedMilliseconds);
+
         Console.WriteLine();
     }
 
-    private static int ParallelSum(int[] array) {
-        var result = array.AsParallel().Sum();
+    private static long ParallelSum(int[] array) {
+        var newArr = array.Select(i => (long)i).ToArray();
+        var result = newArr.AsParallel().Sum();
         return result;
     }
 
-    private static int ThreadSum(int[] array) {
-        int result = 0;
+
+    private static long ThreadSum(int[] array) {
+        long result = 0;
 
         var splitArray = new List<int[]>();
         var z = array.Length / 10;
@@ -67,17 +56,19 @@ class Program {
         for (int i = 0; i < 10; i++)
             splitArray.Add(array.Skip(z * i).Take(z).ToArray());
 
+        List<Thread> threads = new List<Thread>();
         foreach (var tempArray in splitArray) {
             var thread = new Thread(() => result += Sum(tempArray));
+            threads.Add(thread);
             thread.Start();
-            thread.Join();
         }
+        threads.ForEach(x => x.Join());
 
         return result;
     }
 
-    private static int Sum(int[] array) {
-        int result = 0;
+    private static long Sum(int[] array) {
+        long result = 0;
         for (int i = 0; i < array.Length; i++) 
             result += array[i];
         return result;
